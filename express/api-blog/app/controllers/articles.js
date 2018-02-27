26dda4f0e21f0a3ea08ee78693090bfad9ba2f4f
Darana Sukma Vidya <darana.sv@gmail.com>
2018-02-27 19:17:03 +0700

    change json response

--- express/api-blog/app/controllers/articles.js
+++ express/api-blog/app/controllers/articles.js
@@ -1,13 +1,23 @@
 // list article
 exports.list = function(req, res, next) {
     req.getConnection(function(err, connection){
-        connection.query('SELECT * from articles', function(err, rows, fields) {
+        connection.query('SELECT articles.id as articleID, articles.title as articleTitle, articles.description as articleDesc, articles.created_at as articleDate, articles.updated_at as articleUpdate, category.id as catID, category.name as catName from articles LEFT JOIN category ON articles.category = category.id ORDER BY articles.id DESC', function(err, rows, fields) {
             if(err) {
                 return next(err);
             } else {
+                var arr = [];
+                for (let i = 0; i < rows.length; i++) {
+                    arr.push({
+                        title       : rows[i].articleTitle,
+                        category    : rows[i].catName,
+                        description : rows[i].articleDesc.substring(0,78),
+                        created_at  : rows[i].articleDate,
+                        updated_at  : rows[i].articleUpdate
+                    });
+                };
                 res.end(JSON.stringify({
                     status: 200,
-                    data: rows
+                    data: arr
                 }));
             }
         });
@@ -22,7 +32,8 @@ exports.submit = function(req, res) {
         var data = {
             title       : input.title,
             category    : input.category,
-            description : input.description
+            description : input.description,
+            created_at  : new Date()
         };
 
         connection.query('INSERT INTO articles set ? ', data, function(err, rows){
