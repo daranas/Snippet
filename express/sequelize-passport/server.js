933a135367bfa98144848bc08d225faea2029ade
Darana Sukma Vidya <darana.sv@gmail.com>
2018-03-21 21:53:47 +0700

    change gitignore & add module

---
+++ express/sequelize-passport/server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    app = express(),
+    passport = require('passport'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    env = require('dotenv').load();
+
+// body parser
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+// passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function(req, res) {
+  res.send("Hello World");
+})
+
+app.listen(5000, function(err){
+  if(!err)
+    console.log("Site is live")
+  else console.log(err)
+})
