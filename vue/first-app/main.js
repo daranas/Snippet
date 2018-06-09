8ea73582e2eb51201fc26b9cc179f8b7da192944
Darana Sukma Vidya <darana.sv@gmail.com>
2018-06-09 15:11:12 +0700

    try vue-router

---
+++ vue/first-app/main.js
@@ -0,0 +1,16 @@
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
